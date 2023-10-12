adb305cec5b5719628761b0ee399d3df830800a5
xi816 <141260554+xi816@users.noreply.github.com>
2023-10-12 21:27:27 +0300

    Delete scripts directory

--- scripts/info_templates.js
+++
@@ -1,6 +0,0 @@
-els = document.getElementsByClassName("template_stub")
-
-for (let i = 0; i < els.length; i++) {
-  els[i].innerHTML = "Внимание! Эта страница сделана неполностью, и требует дополнения!";
-}
-
