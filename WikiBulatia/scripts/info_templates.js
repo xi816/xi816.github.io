2c27756bbd52d315c9bde6c82d0cf68ae9852f70
xi816 <141260554+xi816@users.noreply.github.com>
2023-10-12 22:45:22 +0300

    Added second page (longwords)

--- WikiBulatia/scripts/info_templates.js
+++ WikiBulatia/scripts/info_templates.js
@@ -1,6 +1,6 @@
 els = document.getElementsByClassName("template_stub")
 
 for (let i = 0; i < els.length; i++) {
-  els[i].innerHTML = "Внимание! Эта страница сделана неполностью, и требует дополнения!";
+  els[i].innerHTML = "Внимание! Эта страница сделана неполностью, и требует дополнения!\n";
 }
 
