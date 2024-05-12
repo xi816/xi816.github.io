3f05da2ed1195222c384ff61442c80f5647d7a10
xi816 <141260554+xi816@users.noreply.github.com>
2024-05-12 23:47:56 +0300

    stub fix e_843829

--- brg-wiki/scripts/info_templates.js
+++ brg-wiki/scripts/info_templates.js
@@ -1,6 +1,6 @@
 els = document.getElementsByClassName("template_stub");
 
 for (let i = 0; i < els.length; i++) {
-  els[i].innerHTML = "не полнъта сторона!\n";
+  els[i].innerHTML = "нє полнъта сторона!\n";
 }
 
